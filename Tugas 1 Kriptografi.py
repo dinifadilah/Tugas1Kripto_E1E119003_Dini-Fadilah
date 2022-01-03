6092196f01d35028a37140a16d3aa03d6c6644f7
dinifadilah <58256020+dinifadilah@users.noreply.github.com>
2022-01-03 21:49:59 +0800

    Add files via upload

---
+++ Tugas 1 Kriptografi.py
@@ -0,0 +1,28 @@
+def enkripsi(plaintext, key):
+    encoded = []
+
+    for i in range(len(plaintext)):
+        xor = ord(plaintext[i]) ^ ord(key[i % len(key)])
+        encoded.append(chr(xor))
+
+    return ''.join(encoded)
+
+def dekripsi(chipertext, key):
+    return enkripsi(chipertext, key)
+
+def main():
+    # fungsi enkripsi
+    plaintext = str(input("Masukkan plaintext: "))
+    key = 'XYZ'
+
+    print("Plaintext: ", plaintext)
+    print("Enkripsi: ", enkripsi(plaintext, key))
+
+    # fungsi dekripsi
+    chipertext = str(input("Masukkan chipertext: "))
+    key = "XYZ"
+
+    print("Chipertext: ", chipertext)
+    print("Dekripsi: ", dekripsi(chipertext, key))
+
+main()
